1858e8613ff4cc783de6a08490036569c42c90f6
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-11 20:33:17 +0300

    all pages added to route

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import Navbar from '../components/Navbar'
+import TeamsPlayers from '../pages/TeamsPlayers'
+import Comparison from '../pages/Comparison'
+import Favourites from '../pages/Favourites'
 
 function AppRouter() {
 	return (
 		<Router>
 			<Navbar />
+			<Switch>
+				<Route path="/comparison" component={Comparison} />
+				<Route path="/favourites" component={Favourites} />
+				<Route path="/" component={TeamsPlayers} />
+			</Switch>
 		</Router>
 	)
 }
