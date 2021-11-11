69c50ec4c273c928f2765c5f076f122275f742d9
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-12 01:52:26 +0300

    functions created

---
+++ src/utils/functions.js
@@ -0,0 +1,21 @@
+export const circleColor = (role) => {
+	switch (role) {
+		case "GKP":
+			return "2px solid yellow"
+		case "DEF":
+			return "2px solid blue"
+		case "MID":
+			return "2px solid green"
+		case "FWD":
+			return "2px solid red"
+		default:
+			break;
+	}
+}
+
+
+export const calculateAge = (birthDate) => {
+	const year = Number(new Date().getFullYear())
+	const birthYear = Number(birthDate.slice(0, 4))
+	return (year - birthYear)
+}
