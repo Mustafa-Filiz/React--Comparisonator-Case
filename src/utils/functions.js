6aaee6e959bbfa25c13e000e179463b6b4c37251
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-12 02:24:50 +0300

    flag bug fixed

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,21 +1,24 @@
 export const circleColor = (role) => {
-	switch (role) {
-		case "GKP":
-			return "2px solid yellow"
-		case "DEF":
-			return "2px solid blue"
-		case "MID":
-			return "2px solid green"
-		case "FWD":
-			return "2px solid red"
-		default:
-			break;
-	}
-}
-
+    switch (role) {
+        case 'GKP':
+            return '2px solid yellow';
+        case 'DEF':
+            return '2px solid blue';
+        case 'MID':
+            return '2px solid green';
+        case 'FWD':
+            return '2px solid red';
+        default:
+            break;
+    }
+};
 
 export const calculateAge = (birthDate) => {
-	const year = Number(new Date().getFullYear())
-	const birthYear = Number(birthDate.slice(0, 4))
-	return (year - birthYear)
-}
+    const year = Number(new Date().getFullYear());
+    const birthYear = Number(birthDate?.slice(0, 4));
+    return year - birthYear;
+};
+
+export const flagArrange = (alphaCode) => {
+    return alphaCode === 'EN' ? 'gb' : alphaCode?.toLowerCase();
+};
