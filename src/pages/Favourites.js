1f2693a61a74c2812817bdc35f521aa06b978073
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-13 01:03:13 +0300

    favourite page styled

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,17 +1,71 @@
-import { Box } from '@mui/system'
-import React, { useContext, useEffect, useState } from 'react'
-import { AppContext } from '../context/AppContext'
+import { DeleteRounded } from '@mui/icons-material';
+import {
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    CardMedia,
+    IconButton,
+    Typography,
+} from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { Box } from '@mui/system';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const useStyles = makeStyles((theme) => {
+    return {
+        container: {
+            width: '90%',
+            display: 'flex',
+            flexWrap: 'wrap',
+            margin: '2rem auto',
+            justifyContent: 'center',
+        },
+        card: {
+            width: '15rem',
+            height: '25rem',
+            margin: '1rem',
+        },
+    };
+});
 
 function Favourites() {
-	const [favPlayers, setFavPlayers] = useState([])
-	const {favourites} = useContext(AppContext)
+    const { favourites, toggleFavourites } = useContext(AppContext);
+    const classes = useStyles();
 
-
-	return (
-		<Box>
-
-		</Box>
-	)
+    return (
+        <Box className={classes.container}>
+            {favourites.length ? (
+                favourites.map((favourite) => {
+                    return (
+                        <Card key={favourite.id} className={classes.card}>
+                            <CardMedia
+                                component="img"
+                                image={favourite.image}
+                            />
+                            <CardHeader
+                                title={favourite.shortName}
+                                subheader={favourite.role.name}
+                            />
+                            <CardActions>
+                                <IconButton
+                                    size="large"
+                                    onClick={() => toggleFavourites(favourite)}
+                                >
+                                    <DeleteRounded fontSize="medium" />
+                                </IconButton>
+                            </CardActions>
+                        </Card>
+                    );
+                })
+            ) : (
+                <Typography variant="h3">
+                    Your favourite player list is empty.
+                </Typography>
+            )}
+        </Box>
+    );
 }
 
-export default Favourites
+export default Favourites;
