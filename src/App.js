f9ced45e4b6507180af5dbcf1a0f942298012bdb
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-12 22:03:11 +0300

    app is wrapped with context comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import AppContextProvider from './context/AppContext';
 import AppRouter from './router/AppRouter';
 
 function App() {
-  return (
-    <div className="App">
-      <AppRouter />
-    </div>
-  );
+    return (
+        <div className="App">
+            <AppContextProvider>
+                <AppRouter />
+            </AppContextProvider>
+        </div>
+    );
 }
 
 export default App;
