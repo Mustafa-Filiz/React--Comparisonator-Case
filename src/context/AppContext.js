d62cc0d256e493e483519fe8519ef2ed75631d74
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-12 23:06:46 +0300

    toggle favourite func created

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,13 +1,35 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from 'react';
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
-const AppContextProvider = ({children}) => {
-	return(
-		<AppContext.Provider >
-			{children}
-		</AppContext.Provider>
-	)
-}
+const AppContextProvider = ({ children }) => {
+    const [favourites, setFavourites] = useState([]);
+
+    const toggleFavourites = (id) => {
+        favourites.includes(id)
+            ? setFavourites(favourites.filter((favourite) => favourite !== id))
+            : setFavourites([...favourites, id]);
+    };
+
+    useEffect(() => {
+        const localFavourites = window.localStorage.getItem('favouritePlayers');
+        if (localFavourites.length) {
+            setFavourites(JSON.parse(localFavourites));
+        }
+    }, []);
+
+    useEffect(() => {
+        window.localStorage.setItem(
+            'favouritePlayers',
+            JSON.stringify(favourites)
+        );
+    }, [favourites]);
+
+    return (
+        <AppContext.Provider value={{ favourites, toggleFavourites }}>
+            {children}
+        </AppContext.Provider>
+    );
+};
 
 export default AppContextProvider;
