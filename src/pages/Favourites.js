a8f87d45e7cc6cd4b0c28b8c8738a4961ff560a5
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-13 00:37:34 +0300

    states created

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import { Box } from '@mui/system'
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
 
 function Favourites() {
+	const [favPlayers, setFavPlayers] = useState([])
+	const {favourites} = useContext(AppContext)
+
+
 	return (
-		<div>
-			Favourites
-		</div>
+		<Box>
+
+		</Box>
 	)
 }
 
