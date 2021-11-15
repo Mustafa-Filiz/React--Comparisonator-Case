a981baee51c80fd5cf823a85bf8c1037116e5a05
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-15 09:49:30 +0300

    add button modified

--- src/pages/Comparison.js
+++ src/pages/Comparison.js
@@ -7,21 +7,34 @@ import {
     Menu,
     MenuItem,
     MenuList,
+    Typography,
 } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 import { Box } from '@mui/system';
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
+const useStyles = makeStyles((theme) => {
+    return {
+        container: {
+            width:"100%",
+            height: "100vh",
+            display: 'flex',
+        },
+    };
+});
+
 function Comparison() {
+    const classes = useStyles();
+
     const { favourites } = useContext(AppContext);
 
     const [comparisonList, setComparisonList] = useState([]);
     const [anchorEl, setAnchorEl] = useState(null);
-	const open = Boolean(anchorEl)
+    const open = Boolean(anchorEl);
 
-
-    const handleMenuClick = () => {
-        setAnchorEl(true);
+    const handleMenuClick = (e) => {
+        setAnchorEl(e.currentTarget);
     };
 
     const handleItemClick = (player) => {
@@ -40,30 +53,60 @@ function Comparison() {
     console.log(comparisonList);
 
     return (
-        <Box>
-            <IconButton size="large" onClick={handleMenuClick}>
-                <Add fontSize="large" />
-            </IconButton>
-            <Menu open={open} anchorEl={anchorEl} onClose={() => setAnchorEl(null)}>
-                <MenuList>
-                    {favourites.map((favourite) => {
-                        return (
-                            <MenuItem
-                                onClick={() => handleItemClick(favourite)}
-                                key={favourite.id}
-                            >
-                                <ListItemAvatar>
-                                    <Avatar src={favourite.image} />
-                                </ListItemAvatar>
-                                <ListItemText
-                                    primary={favourite.shortName}
-                                    secondary={favourite.role.name}
-                                />
+        <Box className={classes.container}>
+            <Box>
+                <Typography>Index</Typography>
+                <Typography>Goals</Typography>
+                <Typography>Asists</Typography>
+                <Typography>Shots</Typography>
+                <Typography>Passes</Typography>
+                <Typography>Crosses</Typography>
+                <Typography>Key Passes</Typography>
+                <Typography>Smart Passes</Typography>
+                <Typography>Touch in Box</Typography>
+                <Typography color="yellow">Yellow Cards</Typography>
+                <Typography color="red">Red Cards</Typography>
+            </Box>
+            <Box>
+                <IconButton
+                    size="large"
+                    onClick={handleMenuClick}
+                >
+                    <Add fontSize="large" />
+                </IconButton>
+                <Menu
+                    open={open}
+                    anchorEl={anchorEl}
+                    onClose={() => setAnchorEl(null)}
+                >
+                    <MenuList>
+                        {favourites.length ? (
+                            favourites.map((favourite) => {
+                                return (
+                                    <MenuItem
+                                        onClick={() =>
+                                            handleItemClick(favourite)
+                                        }
+                                        key={favourite.id}
+                                    >
+                                        <ListItemAvatar>
+                                            <Avatar src={favourite.image} />
+                                        </ListItemAvatar>
+                                        <ListItemText
+                                            primary={favourite.shortName}
+                                            secondary={favourite.role.name}
+                                        />
+                                    </MenuItem>
+                                );
+                            })
+                        ) : (
+                            <MenuItem>
+                                <ListItemText primary="Your favourite list is empty." />
                             </MenuItem>
-                        );
-                    })}
-                </MenuList>
-            </Menu>
+                        )}
+                    </MenuList>
+                </Menu>
+            </Box>
         </Box>
     );
 }
