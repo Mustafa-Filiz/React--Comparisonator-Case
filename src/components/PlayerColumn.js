a2d8c3faa4b92744c904dafd62716bb21b9e8b56
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-15 16:05:19 +0300

    updates

--- src/components/PlayerColumn.js
+++ src/components/PlayerColumn.js
@@ -1,68 +1,25 @@
-import React, { useContext, useState } from 'react'
-import { Add } from '@mui/icons-material';
-import {
-    Avatar,
-    IconButton,
-    ListItemAvatar,
-    ListItemText,
-    Menu,
-    MenuItem,
-    MenuList,
-    Typography,
-} from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import { Box } from '@mui/system';
-import { AppContext } from '../context/AppContext';
+import React, { useEffect, useState } from 'react';
+import { Typography } from '@mui/material';
+import axios from 'axios';
 
-const useStyles = makeStyles((theme) => {
-    return {
-        container: {
-            width: '100%',
-            height: '80vh',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'flex-start',
-        },
-        players:{
-            display: "flex",
-            alignItems:"center"
+function PlayerColumn({ id }) {
+    const [playerStats, setPlayerStats] = useState({});
 
-        },
-        emptyBox:{
-            width : "15rem"
-        },
-        playerInfo:{
-            width: "8rem",
-            height: "8rem",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent:"center",
-            alignItems: "center"
-        },
-        headings: {
-            width : "15rem"
-        },
-    };
-});
+    useEffect(() => {
+        axios
+            .get(`https://mock-foooty-api.herokuapp.com/players/${id}/stats`)
+            .then((res) => setPlayerStats(res.data.stats));
+    }, [id]);
 
-function PlayerColumn({comparisonList}) {
-    const classes = useStyles();
+    // console.log(playerStats)
 
-	return (
-		<Box className={classes.players}>
-                {comparisonList.length
-                    ? comparisonList.map((player) => {
-                          return (
-                              <Box className={classes.playerInfo}>
-                                  <Avatar sx={{ width: 80, height: 80 }} src={player.image} />
-                                  <Typography variant="h5">{player.shortName}</Typography>
-                              </Box>
-                          );
-                      })
-                    : null}
-            </Box>
-	)
+    return (
+        <>
+            {Object.values(playerStats).map((stat, index) => (
+                <Typography key={index} variant="h6">{Math.round(Number(stat))}</Typography>
+            ))}
+        </>
+    );
 }
 
-export default PlayerColumn
+export default PlayerColumn;
