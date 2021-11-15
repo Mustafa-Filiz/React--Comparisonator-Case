05163b3e52cc737826d4c5b76958efded3893dfa
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-15 12:58:54 +0300

    updates

--- src/components/PlayerColumn.js
+++ src/components/PlayerColumn.js
@@ -1,10 +1,67 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { Add } from '@mui/icons-material';
+import {
+    Avatar,
+    IconButton,
+    ListItemAvatar,
+    ListItemText,
+    Menu,
+    MenuItem,
+    MenuList,
+    Typography,
+} from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { Box } from '@mui/system';
+import { AppContext } from '../context/AppContext';
 
-function PlayerColumn() {
+const useStyles = makeStyles((theme) => {
+    return {
+        container: {
+            width: '100%',
+            height: '80vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'flex-start',
+        },
+        players:{
+            display: "flex",
+            alignItems:"center"
+
+        },
+        emptyBox:{
+            width : "15rem"
+        },
+        playerInfo:{
+            width: "8rem",
+            height: "8rem",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent:"center",
+            alignItems: "center"
+        },
+        headings: {
+            width : "15rem"
+        },
+    };
+});
+
+function PlayerColumn({comparisonList}) {
+    const classes = useStyles();
+
 	return (
-		<div>
-			
-		</div>
+		<Box className={classes.players}>
+                {comparisonList.length
+                    ? comparisonList.map((player) => {
+                          return (
+                              <Box className={classes.playerInfo}>
+                                  <Avatar sx={{ width: 80, height: 80 }} src={player.image} />
+                                  <Typography variant="h5">{player.shortName}</Typography>
+                              </Box>
+                          );
+                      })
+                    : null}
+            </Box>
 	)
 }
 
