0bdfca312886e0021bdd160542a06d40e0ab600b
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-15 09:49:12 +0300

    stats modified

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -12,17 +12,19 @@ const useStyles = makeStyles((theme) => {
         },
         slider: {
             width: '30% !important',
-			display: 'flex',
-			flexDirection: "column",
-			justifyContent : "space-evenly",
-			alignItems: "center",
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-evenly',
+            alignItems: 'center',
             '& .MuiSlider-thumb': {
                 display: 'none ',
             },
         },
-		statsFirst:{
-
-		},
+        stats: {
+            width: '30%',
+            display: 'flex',
+            justifyContent: 'space-between',
+        },
     };
 });
 
@@ -36,28 +38,60 @@ function Stats({ id }) {
             .then((res) => setStats(res.data.stats));
     }, []);
 
+    console.log(Object.values(stats));
+
     return (
         <Box className={classes.dropdown}>
             <Box className={classes.slider}>
-                <Slider value={Math.round(stats.index)} max={500} sx={{height: "1rem"}} />
-                <Typography sx={{color: "#2196F3", fontSize: "2.5rem", fontWeight: 600}}>
+                <Slider
+                    value={Math.round(stats.index)}
+                    max={500}
+                    sx={{ height: '1rem' }}
+                />
+                <Typography
+                    sx={{
+                        color: '#2196F3',
+                        fontSize: '2.5rem',
+                        fontWeight: 600,
+                    }}
+                >
                     {stats.index ? Math.round(stats.index) : 'No Data'}
                 </Typography>
-                <Typography sx={{color: "#2196F3", fontSize: "1rem"}}>COMPARISONATOR INDEX</Typography>
+                <Typography sx={{ color: '#2196F3', fontSize: '1rem' }}>
+                    COMPARISONATOR INDEX
+                </Typography>
             </Box>
-            <Box className={classes.statsFirst}>
-                <Typography>Goals</Typography>
-                <Typography>Asists</Typography>
-                <Typography>Shots</Typography>
-                <Typography>Passes</Typography>
-                <Typography>Crosses</Typography>
+            <Box className={classes.stats}>
+                <Box>
+                    <Typography>Goals</Typography>
+                    <Typography>Asists</Typography>
+                    <Typography>Shots</Typography>
+                    <Typography>Passes</Typography>
+                    <Typography>Crosses</Typography>
+                </Box>
+                <Box>
+                    {Object.values(stats)
+                        .slice(1, 6)
+                        .map((item) => {
+                            return <Typography>{item}</Typography>;
+                        })}
+                </Box>
             </Box>
-            <Box>
-                <Typography>Key Passes</Typography>
-                <Typography>Smart Passes</Typography>
-                <Typography>Touch in Box</Typography>
-                <Typography>Yellow Cards</Typography>
-                <Typography>Red Cards</Typography>
+            <Box className={classes.stats}>
+                <Box>
+                    <Typography>Key Passes</Typography>
+                    <Typography>Smart Passes</Typography>
+                    <Typography>Touch in Box</Typography>
+                    <Typography color="yellow">Yellow Cards</Typography>
+                    <Typography color="red">Red Cards</Typography>
+                </Box>
+                <Box>
+                    {Object.values(stats)
+                        .slice(6)
+                        .map((item) => {
+                            return <Typography>{item}</Typography>;
+                        })}
+                </Box>
             </Box>
         </Box>
     );
