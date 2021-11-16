bd55bb21c34247ecf40e823633f407c252868952
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-16 14:01:58 +0300

    responsiveness added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import AppContextProvider from './context/AppContext';
+import ThemeContextProvider from './context/ThemeContext';
 import AppRouter from './router/AppRouter';
 
 function App() {
     return (
         <div className="App">
-            <AppContextProvider>
-                <AppRouter />
-            </AppContextProvider>
+            <ThemeContextProvider>
+                <AppContextProvider>
+                    <AppRouter />
+                </AppContextProvider>
+            </ThemeContextProvider>
         </div>
     );
 }
